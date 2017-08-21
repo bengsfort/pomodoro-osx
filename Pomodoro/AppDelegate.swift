b3d2663875c7a38ffe8fbe24fe1da61a451ca21e
Matt Bengston <mattb@unity3d.com>
2017-08-21 14:24:30 +0300

    Initial Commit

---
+++ Pomodoro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pomodoro
+//
+//  Created by Matt Bengston on 21/08/2017.
+//  Copyright © 2017 Bengsfort. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
